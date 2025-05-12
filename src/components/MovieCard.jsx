31f885fcc59bf00b5b12bc58f25468e50893d815
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-05-12 17:46:10 +0200

    feat: add voto medio

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 
 export default function MovieCard(props) {
-    const { id, title, director, imagePath, abstract } = props.data;
+    const { id, title, director, imagePath, abstract, voto_medio } = props.data;
 
     return (
         <div className='card-movie'>
             <img src={imagePath} alt={title} />
             <h3>{title}</h3>
+            <p>Media Recensione: {voto_medio}</p>
             <p>Director: <strong>{director}</strong> </p>
             <p>{abstract}</p>
             <Link className='btn-info' to={`/movies/${id}`}>Info Film</Link>
