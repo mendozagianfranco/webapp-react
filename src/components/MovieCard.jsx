b65e5263e01f50dce9fb20c1b23c87c426a3971c
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-05-12 16:46:10 +0200

    feat: correct data MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 
 export default function MovieCard(props) {
-    const { id, title, imagePath, abstract } = props.data;
+    const { id, title, director, imagePath, abstract } = props.data;
 
     return (
         <div>
             <img src={imagePath} alt={title} />
             <h3>{title}</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse harum rem facilis incidunt nemo deserunt distinctio laboriosam consequuntur nesciunt, eligendi sed omnis corporis, ea quae in dolor labore quidem sit!</p>
+            <p>Director: <strong>{director}</strong> </p>
+            <p>{abstract}</p>
             <Link to={`/movies/${id}`}>Info Film</Link>
         </div>
     );
