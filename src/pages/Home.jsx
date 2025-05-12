deed2541e6bf9cfad16524d6fd69aeae80195ac7
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-05-12 17:20:45 +0200

    feat: add search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,21 @@ import MovieCard from '../components/MovieCard';
 
 export default function Home() {
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('');
 
     const endpoint = 'http://127.0.01:3000/api/movies';
     function fetchMovies() {
-        axios.get(endpoint)
+        axios.get(endpoint, { params: { search } })
             .then(res => {
                 setMovies(res.data);
             })
             .catch(err => console.log(err));
+    }
+
+    function searchMovies(e) {
+        e.preventDefault();
+        fetchMovies();
+
     }
 
     useEffect(fetchMovies, []);
@@ -21,9 +28,9 @@ export default function Home() {
             <h1>Movies</h1>
             <div className='subtitle'>
                 <h2>Best movies in the class142</h2>
-                <form >
+                <form onSubmit={searchMovies}>
                     <label className='label-search'>Cerca</label>
-                    <input className='form-control' type="text" placeholder='Cerca' />
+                    <input className='form-control' value={search} onChange={(e) => { setSearch(e.target.value); }} type="text" placeholder='Cerca' />
                     <button type='submit' className='btn-info'>Cerca</button>
                 </form>
             </div>
