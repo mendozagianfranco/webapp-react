adbc04dfe8d328e09b2d9e3d1ecfbdeb3a129f3f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-05-13 17:03:10 +0200

    feat: add info in singlemovie

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 export default function SingleMovie() {
-
     const { id } = useParams();
+    const [movie, setMovie] = useState({});
+    const endpoint = `http://127.0.01:3000/api/movies/${id}`;
+
+    function getMovie() {
+        axios.get(endpoint)
+            .then(res => {
+                setMovie(res.data);
+            });
+    }
+
+    useEffect(getMovie, []);
+
     return (
-        <div className='container'>
-            <h1>Movie con id:{id}</h1>
+        <div className='container container-movie-detail'>
+            <h1>{movie.title}</h1>
+            <img src={movie.image} alt="" />
+            <p>{movie.abstract}</p>
+            <div className='container-reviews'>
+                {movie.reviews?.length ? movie.reviews.map(review => (
+                    <div key={review.id}>
+                        {review.name}
+                    </div>
+                )) : <div>Nessuna Recensione</div>}
+            </div>
         </div>
     );
 }
